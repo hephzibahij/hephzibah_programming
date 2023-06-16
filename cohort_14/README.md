332eaea080bf95c484646498cae134e2c2b2ff76
Hephzibah <hephzibahijeje89@gmail.com>
2023-06-16 09:08:21 -0700

    my first attempt

---
+++ cohort_14/README.md
@@ -0,0 +1,5 @@
+cohort_14
+Task done by Ijeje Hephzibah
+iphne 14
+iphone 6
+iphone 8
---
+++ cohort_14/main.h
@@ -0,0 +1,12 @@
+#ifndef cohort_14
+#define cohort_14
+
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char c);
+int hephzibah14(char **head);
+int loin6(char **head);
+int iphone8(char **head);
+
+#endif
